97709b5d2cf47e8f23965079d4c2a3b010f4380b
David Albert <david@bronto.com>
2013-09-23 21:17:24 -0400

    removing superfluous file

--- src/mort/amortize.clj
+++
@@ -1,8 +0,0 @@
-
-(defn monthlyInterestRate [interestRate] (/ interestRate 12))
-(defn rateToPercent [rate] (/ rate 100))
-
-(defn oneMonthOfInterest [balance interestRate]
-  (* balance (rateToPercent (monthlyInterestRate interestRate))))
-
-(oneMonthOfInterest 180000 4.875)
