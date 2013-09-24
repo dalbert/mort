f85013ecc71f4fb8f33c91b0030f7871b2f6cc78
David Albert <david@bronto.com>
2013-09-24 11:53:22 -0400

    new function, calcs interest accrual for arbitrary number of years

--- src/mort/core.clj
+++ src/mort/core.clj
@@ -17,6 +17,11 @@
   [rate]
   (/ rate 100))
 
+(defn years-to-months
+  "Multiples by 12."
+  [years]
+  (* years 12))
+
 (defn one-month-of-interest
   "Calculates the amount of interest that would accrue in one month."
   [balance interest-rate]
@@ -49,6 +54,19 @@
   "Iteratively calculates the total interest accrued in 12 months with principle+interest payments being made as required by the bank."
   [starting-balance interest-rate monthly-payment]
   (loop [iteration 12 balance starting-balance accrual 0 payment monthly-payment]
+    (if (> iteration 0)
+      (recur
+       (dec iteration)
+       (- balance (principle-payment-left-after-interest payment (one-month-of-interest balance interest-rate)))
+       (+ accrual (one-month-of-interest balance interest-rate))
+       payment)
+      accrual
+    )))
+
+(defn years-of-accrual-with-required-payments
+  "Iteratively calculates the total interest accrued in the given number of years with principle+interest payments being made as required by the bank."
+  [years starting-balance interest-rate monthly-payment]
+  (loop [iteration (years-to-months years) balance starting-balance accrual 0 payment monthly-payment]
     (if (> iteration 0)
       (recur
        (dec iteration)
