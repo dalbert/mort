29179d7e4453a736249f616eb6298c72d8e6c4d5
David Albert <david@bronto.com>
2013-09-24 23:07:29 -0400

    some new funcs. just started adding more component funcs to do a refactor of the higher level funcs to make them iterate over years instead of months

--- src/mort/core.clj
+++ src/mort/core.clj
@@ -7,6 +7,8 @@
   [& args]
   (println "Hello, World!"))
 
+;; below: reusable, component functions
+
 (defn monthly-interest-rate
   "Divides by 12. Pass in the annual interest rate."
   [interest-rate]
@@ -22,12 +24,31 @@
   [years]
   (* years 12))
 
+(defn months-to-years
+  "Divides by 12"
+  [months]
+  (/ months 12))
+
 (defn one-month-of-interest
   "Calculates the amount of interest that would accrue in one month."
   [balance interest-rate]
   (if (> balance 0)
     (* balance (rate-to-percent (monthly-interest-rate interest-rate)))
     0))
+
+(defn new-balance-after-payment
+  "Calculates the new loan balance after one month's payment is made."
+  [balance interest-rate required principle bonus]
+  (let [new-balance
+         (- balance
+            (principle-payment-left-after-interest required (one-month-of-interest balance interest-rate))
+            principle
+            bonus)]
+     (if (< new-balance 0) 0 new-balance)))
+
+(float (new-balance-after-payment 100000 5 750 500 100))
+
+;; below: one-off functions
 
 (defn one-year-of-compounded-accrual-with-no-payments
   "Iteratively calculates the total interest accrued in 12 compounding months."
@@ -53,7 +74,8 @@
   (- payment interest))
 
 (defn one-year-of-accrual-with-required-payments
-  "Iteratively calculates the total interest accrued in 12 months with principle+interest payments being made as required by the bank."
+  "Iteratively calculates the total interest accrued in 12 months with principle+interest
+  payments being made as required by the bank."
   [starting-balance interest-rate monthly-payment]
   (loop [iteration 12 balance starting-balance accrual 0]
     (if (> iteration 0)
@@ -65,25 +87,68 @@
     )))
 
 (defn years-of-accrual-with-required-payments
-  "Iteratively calculates the total interest accrued in the given number of years with principle+interest payments being made as required by the bank."
+  "Iteratively calculates the total interest accrued in the given number of years
+  with principle+interest payments being made as required by the bank."
   [years starting-balance interest-rate monthly-payment]
-  (loop [iteration (years-to-months years) balance starting-balance accrual 0]
-    (if (> iteration 0)
+  (loop [iteration 0 balance starting-balance accrual 0]
+    (if (< iteration (years-to-months years))
       (recur
-       (dec iteration)
+       (inc iteration)
        (- balance (principle-payment-left-after-interest monthly-payment (one-month-of-interest balance interest-rate)))
        (+ accrual (one-month-of-interest balance interest-rate)))
       accrual
     )))
 
 (defn years-of-accrual-with-scheduled-principle-payments
-  "Iteratively calculates the total interest accrued in the given number of years with additional principle payments made each month."
+  "Iteratively calculates the total interest accrued in the given number of years
+  with additional principle payments made each month."
   [years starting-balance interest-rate monthly-payment monthly-principle-payment]
-  (loop [iteration (years-to-months years) balance starting-balance accrual 0]
-    (if (> iteration 0)
+  (loop [iteration 0 balance starting-balance accrual 0]
+    (if (< iteration (years-to-months years))
       (recur
-       (dec iteration)
+       (inc iteration)
        (- balance (principle-payment-left-after-interest monthly-payment (one-month-of-interest balance interest-rate)) monthly-principle-payment)
        (+ accrual (one-month-of-interest balance interest-rate)))
       accrual
+    )))
+
+(defn years-of-accrual-with-scheduled-principle-payments-and-bonus-payments
+  "Iteratively calculates the total interest accrued in the given number of years
+  with additional principle payments made each month and also a map of one-time
+  payments made in specific months during the course of the repayment period."
+  [years starting-balance interest-rate monthly-payment monthly-principle-payment bonus-payments-map]
+  (loop [iteration 0 balance starting-balance accrual 0]
+    (if (and (< iteration (years-to-months years)) (> balance 0))
+      (recur
+       (inc iteration)
+       (let [new-balance
+             (- balance
+              (principle-payment-left-after-interest monthly-payment (one-month-of-interest balance interest-rate))
+              monthly-principle-payment
+              (get bonus-payments-map iteration 0))]
+         (if (< new-balance 0) 0 new-balance))
+       (+ accrual (one-month-of-interest balance interest-rate)))
+      {:ending-balance balance :interest-paid accrual :years (float (months-to-years iteration))}
+    )))
+
+(defn years-of-accrual-with-scheduled-principle-payments-and-bonus-payments
+  "Iteratively calculates the total interest accrued in the given number of years
+  with additional principle payments made each month and also a map of one-time
+  payments made in specific months during the course of the repayment period. Then
+  it also accounts for special tax breaks on mortgage interest."
+  [years starting-balance interest-rate monthly-payment monthly-principle-payment bonus-payments-map tax-refund-settings]
+  (loop [iteration 0 balance starting-balance accrual 0]
+    ;; TODO: make this use funcs that do a year's-worth at a time. Will need a new func that calcs new balance after a year of payments
+    (if (and (< iteration years) (> balance 0))
+      (if (= (mod iteration 12) 0))
+      (recur
+       (inc iteration)
+       (let [new-balance
+             (- balance
+              (principle-payment-left-after-interest monthly-payment (one-month-of-interest balance interest-rate))
+              monthly-principle-payment
+              (get bonus-payments-map iteration 0))]
+         (if (< new-balance 0) 0 new-balance))
+       (+ accrual (one-month-of-interest balance interest-rate)))
+      {:ending-balance balance :interest-paid accrual :years (float (months-to-years iteration))}
     )))
